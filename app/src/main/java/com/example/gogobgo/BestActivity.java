0e4da91e1487060d870006f8230a9a841519e2e5
Mikkel <mik38mik@gmail.com>
2022-04-27 10:28:50 +0200

    NEw things

---
+++ app/src/main/java/com/example/gogobgo/BestActivity.java
@@ -0,0 +1,78 @@
+package com.example.gogobgo;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BestActivity extends AppCompatActivity {
+
+    TextView tv_score,winStar;
+
+    int lastScore,starScore;
+    int starsWon;
+    int best1, best2, best3;
+
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_score = (TextView) findViewById(R.id.step_view);
+        winStar = (TextView) findViewById(R.id.step_distance);
+
+        //Load old scores
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        lastScore = preferences.getInt("lastScore", 0);
+        best1 = preferences.getInt("best1", 0);
+        best2 = preferences.getInt("best2", 0);
+        best3 = preferences.getInt("best3", 0);
+        starScore = preferences.getInt("starScore",0);
+
+        //Replaces highscore beats
+        if(lastScore > best3){
+            best3 = lastScore;
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("best3",  best3);
+            editor.apply();
+        }
+        if(lastScore > best2){
+            int temp = best2;
+            best2 = lastScore;
+            best3 = temp;
+           SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("best3",  best3);
+            editor.putInt("best2",  best2);
+            editor.apply();
+        }
+        if(lastScore > best1){
+            int temp = best1;
+            best1 = lastScore;
+            best2 = temp;
+           SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("best2",  best2);
+            editor.putInt("best1",  best1);
+            editor.apply();
+        }
+
+        for(int starScore = 130;starScore<lastScore;starScore+=130){
+            starsWon++;
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("starsWon", starsWon);
+            editor.apply();
+        }
+//Display score
+        tv_score.setText("LAST SCORE: "+ lastScore + "\n" +  "BEST1: " + best1 + "\n" +  "BEST2: " + best2 + "\n" +  "BEST3: " + best3);
+        winStar.setText("Stars: "+starsWon);
+
+    }
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/gogobgo/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.gogobgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private TextView tv_score,stepDistance;
+    private double MagnitudePrevious = 0;
+    int steppieCount = 0;
+    boolean running;
+    Button b_end;
+
+    int score = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        running = true;
+        setContentView(R.layout.activity_main);
+        tv_score = (TextView) findViewById(R.id.step_view);
+        stepDistance =findViewById(R.id.step_distance);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+
+
+
+        b_end = (Button) findViewById(R.id.b_end);
+
+
+
+        b_end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("lastScore", steppieCount);
+                editor.apply();
+
+                Intent intent = new Intent(getApplicationContext(),BestActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        if(running){
+
+            if(sensorEvent != null){
+                float x_accel = sensorEvent.values[0];
+                float y_accel = sensorEvent.values[1];
+                float z_accel = sensorEvent.values[2];
+
+                double Magnitude = Math.sqrt(x_accel*x_accel+y_accel*y_accel+z_accel*z_accel);
+                double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                MagnitudePrevious = Magnitude;
+
+                if(MagnitudeDelta > 6){
+                    steppieCount++;
+                }
+                tv_score.setText(String.valueOf(steppieCount));
+            }
+           stepDistance.setText(String.valueOf(distanceCalc(steppieCount)));
+        }
+
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!=null) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+        }else{
+            Toast.makeText(this,"Sensor Not found",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!=null){
+            sensorManager.unregisterListener(this,sensor);
+
+        }
+    }
+
+    public float distanceCalc(int steppieCount){
+        float distance = (float)(steppieCount*78)/(float)10000;
+        return distance;
+        }
+
+}
+
